1c5bb366ba7fe27c8657df43042fb054ae87eb04
Jacob Moen <jacmoe.dk@gmail.com>
2016-01-15 10:17:15 +0100

    utility function to check if exec is enabled

--- components/Utility.php
+++ components/Utility.php
@@ -2,34 +2,39 @@
 
 namespace jacmoe\mdpages\components;
 
-/**
-* static method to get files by directory and file filter
-*
-* @param        $directory
-* @param string $filter
-*
-* @return array
-*/
 class Utility {
 
-  public static function getFiles($directory, $filter = '\jacmoe\mdpages\components\GeneralFileFilterIterator') {
-    if(!file_exists($directory)) {
-      return null;
+    public static function exec_enabled() {
+        $disabled = explode(', ', ini_get('disable_functions'));
+        return !in_array('exec', $disabled);
     }
-    $files = new $filter(
-    new \RecursiveIteratorIterator(
-    new \RecursiveDirectoryIterator(
-    $directory,
-    \RecursiveDirectoryIterator::FOLLOW_SYMLINKS
-    )
-    )
-  );
-  $result = array();
-  foreach ($files as $file) {
-    /** @var \SplFileInfo $file */
-    $result[] = $file->getPathname();
-  }
 
-  return $result;
-}
+    /**
+    * static method to get files by directory and file filter
+    *
+    * @param        $directory
+    * @param string $filter
+    *
+    * @return array
+    */
+    public static function getFiles($directory, $filter = '\jacmoe\mdpages\components\GeneralFileFilterIterator') {
+        if(!file_exists($directory)) {
+            return null;
+        }
+        $files = new $filter(
+            new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator(
+            $directory,
+            \RecursiveDirectoryIterator::FOLLOW_SYMLINKS
+            )
+            )
+        );
+        $result = array();
+        foreach ($files as $file) {
+            /** @var \SplFileInfo $file */
+            $result[] = $file->getPathname();
+        }
+
+        return $result;
+    }
 }
