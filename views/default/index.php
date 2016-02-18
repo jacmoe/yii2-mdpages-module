4c37fd7f404b347b216fb23dec684c0040ab868a
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-18 07:41:47 +0100

    Preparing to move this project forward

--- views/default/index.php
+++ views/default/index.php
@@ -3,22 +3,12 @@
 $this->title = 'Pages';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="mdpages-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
-</div>
 <?php
 $metaParser = new jacmoe\mdpages\components\Meta;
 $files = jacmoe\mdpages\components\Utility::getFiles(\Yii::getAlias('@pages'));
-if(!is_null($files)) {
+if(is_null($files)) {
+    echo 'no pages here';
+} else {
   foreach($files as $file) {
       echo $file . "<br>";
       $metatags = $metaParser->parse(file_get_contents($file));
