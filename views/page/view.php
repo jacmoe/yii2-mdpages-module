3665160aaf313b52a79aa4618e7b262577af9f11
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-19 17:05:12 +0100

    Updated views

---
+++ views/page/view.php
@@ -0,0 +1,10 @@
+<?php
+/* @var $this yii\web\View */
+$this->title = $metatags['title'];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<?php
+echo '<pre>';
+print_r($metatags);
+echo '</pre>';
+?>
