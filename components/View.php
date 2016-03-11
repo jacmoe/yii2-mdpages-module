51fd45b36732d00c76e678c420ec3747fe9ce84d
Jacob Moen <jacmoe.dk@gmail.com>
2016-03-11 20:43:47 +0100

    Fixing the View component .. (reverted from commit d511f15c47715e04bb4f22726adc444be4a3ca70)

--- components/View.php
+++ components/View.php
@@ -1,6 +1,5 @@
 <?php
-namespace app\components;
-
+namespace jacmoe\mdpages\components;
 /*
 * This file is part of
 *     the yii2   _
@@ -14,21 +13,19 @@ namespace app\components;
 *	Copyright (c) 2016 Jacob Moen
 *	Licensed under the MIT license
 */
-use Yii;
-use yii\base\InvalidCallException;
 
 /**
  * This View class overrides render and findViewFile
- * to use the theme view files
+ * to be able to use views in the theme pathmap
+ * that does not exist in the overridden module
  */
 class View extends \yii\web\View {
 
     /**
-     * [render description]
-     * @param  [type] $view   [description]
-     * @param  array  $params [description]
-     * @return [type]         [description]
-     */
+     * Override to always pass the current context to render
+     * https://github.com/yiisoft/yii2/issues/4382
+	 * @inheritDoc
+	 */
     public function render($view, $params = array(), $context = null)
     {
         if ($context === null) {
@@ -39,48 +36,19 @@ class View extends \yii\web\View {
     }
 
     /**
-     * [findViewFile description]
-     * @param  [type] $view    [description]
-     * @param  [type] $context [description]
-     * @return [type]          [description]
-     */
+     * If the view is not found by normal means
+     * then use the theme pathmap to find it.
+	 * @inheritDoc
+	 */
     protected function findViewFile($view, $context = null)
     {
-        if (strncmp($view, '@', 1) === 0) {
-            // e.g. "@app/views/main"
-            $file = Yii::getAlias($view);
-        } elseif (strncmp($view, '//', 2) === 0) {
-            // e.g. "//layouts/main"
-            $file = Yii::$app->getViewPath() . DIRECTORY_SEPARATOR . ltrim($view, '/');
-        } elseif (strncmp($view, '/', 1) === 0) {
-            // e.g. "/site/index"
-            if (Yii::$app->controller !== null) {
-                $file = Yii::$app->controller->module->getViewPath() . DIRECTORY_SEPARATOR . ltrim($view, '/');
-            } else {
-                throw new InvalidCallException("Unable to locate view file for view '$view': no active controller.");
-            }
-        } elseif ($context instanceof ViewContextInterface) {
-            $file = $context->getViewPath() . DIRECTORY_SEPARATOR . $view;
-        } elseif (($currentViewFile = $this->getViewFile()) !== false) {
-            $file = dirname($currentViewFile) . DIRECTORY_SEPARATOR . $view;
-        } else {
-            //TODO: figure out why we even end up here.. ?
-            $file = $context->getViewPath() . DIRECTORY_SEPARATOR . $view;
-            //throw new InvalidCallException("Unable to resolve view file for view '$view': no active view context.");
-        }
-
-        if (pathinfo($file, PATHINFO_EXTENSION) !== '') {
-            return $file;
-        }
-        $path = $file . '.' . $this->defaultExtension;
+        $path = $view . '.' . $this->defaultExtension;
         if ($this->theme !== null) {
             $path = $this->theme->applyTo($path);
         }
-        if ($this->defaultExtension !== 'php' && !is_file($path)) {
-            $path = $file . '.php';
-        }
+        $viewfile = parent::findViewFile($path, $context);
 
-        return $path;
+        return $viewfile;
     }
 
 }
